cb8b01182ac1bcac2bd0d5c67150ceea96b33ec4
ouemdzi <sashka.hf@gmail.com>
2024-04-20 14:04:09 +0200

    task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+    box.style.cssText = `
+        width: ${size}px;
+        height: ${size}px;
+        background-color: ${getRandomHexColor()};
+        margin-bottom: 10px;
+      `;
+    fragment.appendChild(box);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
